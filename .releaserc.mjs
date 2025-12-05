8c98abadeaefd8a5abc5274a4ee2ba06adcdbbd3
drlkf <drlkf@drlkf.net>
2025-12-05 20:44:50 +0100

    ci: add semantic-release config

---
+++ .releaserc.mjs
@@ -0,0 +1,51 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ["main"],
+    tagFormat: "${version}",
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                "preset": "conventionalcommits",
+                "releaseRules": [
+                    { "type": "docs", "release": "patch" }
+                ]
+            }
+        ],
+        "@semantic-release/release-notes-generator",
+        [
+            "semantic-release-mirror-version",
+            {
+                "fileGlob": "*(package.yaml|fused-effects-logger.cabal)",
+                "placeholderRegExp": /(?<=\.)[0-9]+\.[0-9]+\.[0-9]+/g
+            }
+        ],
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md",
+                changelogTitle: "Changelog"
+            }
+        ],
+        [
+            "@semantic-release/exec",
+            {
+                "generateNotesCmd": "./bin/run-dch ${lastRelease.version} ${nextRelease.version}"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": [
+                    "package.yaml",
+                    "fused-effects-logger.cabal",
+                    "CHANGELOG.md",
+                    "debian/changelog",
+                ],
+                "message": "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
+            }
+        ]
+    ]
+};
